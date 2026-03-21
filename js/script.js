69665796be9d3f8eb6b30adc6fb3b343eacae649
lumustephen2020-sys <lumustephen2020@gmail.com>
2026-03-21 10:59:14 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ const products = [
         name: "Classic Sneakers",
         price: 110000, 
         category: "Fashion",
-        image: "images/pexels-zaktech90-1027130.jpg", 
+        image: "images/download.jpg", 
         description: "Comfortable Shoes"
     },
     {
